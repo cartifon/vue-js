bf3a6ec5dda45a2d9e12183c0858ff6595fa23c6
cartifon <ciceroartifon@gmail.com>
2017-09-23 20:53:06 -0400

    Responding the excercise.

--- Exercise 3/app.js
+++ Exercise 3/app.js
@@ -2,5 +2,18 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        watch: {
+            value: function (newValue) {
+                var vm = this;
+                setTimeout(function () {
+                    vm.value = 0;
+                }, 5000);
+            }
+        },
+        computed: {
+            result: function () {
+                return this.value == 37 ? 'Done' : 'not there yet';
+            }
         }
     });
