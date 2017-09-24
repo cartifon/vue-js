c2590f14739a90fea3e1533732989c25cc0cf156
cartifon <ciceroartifon@gmail.com>
2017-09-23 21:40:05 -0400

    Exercise 4, Done

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,11 +1,29 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    isHighlight: true,
+    classes: ['class1', 'class2', 'class3'],
+    className: '',
+    hasClass: true,
+    bgColor: '',
+    progressBar: {
+      height: '20px',
+      width: 0,
+      backgroundColor: 'red',
+      widthValue: 0
+    }
   },
   methods: {
-    startEffect: function() {
-    
+    startEffect: function () {
+      this.isHighlight = !this.isHighlight;
+      return this.isHighlight;
+    },
+    startProgress: function () {
+      var vm = this;
+      setInterval(function () {
+        vm.progressBar.widthValue += 5 ;
+        vm.progressBar.width = vm.progressBar.widthValue + 'px';
+      }, 2000);
     }
   }
 });
